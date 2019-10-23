edb8fca1e49723d4551e7061d7d848736ece4224
serycjon <jonas.serych@gmail.com>
2019-10-23 14:12:59 +0200

    add backward compatibility

--- imapnotify/core.py
+++ imapnotify/core.py
@@ -131,7 +131,10 @@ class Notifier:
     self.boxes[box]['idle'] = asyncio.ensure_future(imap_client.idle())
 
   def _is_new_msg(self, msg):
-    # msg is list of messages
+    if 'EXISTS' in msg:
+      return True
+
+    # if msg is list of messages
     for x in msg:
       if 'EXISTS' in x:
         return True
